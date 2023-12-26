360f7632625759eb04658f4a7bdab3c6b6c659b9
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-26 20:18:27 +0900

    Title: 체크판위에서 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231226/체크판위에서 2/on-the-checkboard-2.py
+++ 231226/체크판위에서 2/on-the-checkboard-2.py
@@ -8,11 +8,14 @@ cnt = 0
 
 standard = arr[0][0]
 
-for i in range(1,r-1):
-    for j in range(1,c-1):
-        if arr[i][j] != standard:
-            for a in range(i+1,r-1):
-                for b in range(j+1,c-1):
-                    if arr[a][b] == standard:
-                        cnt += 1
-print(cnt)
+if standard == arr[r-1][c-1]:
+    print(0)
+else:
+    for i in range(1,r-1):
+        for j in range(1,c-1):
+            if arr[i][j] != standard:
+                for a in range(i,r-1):
+                    for b in range(j,c-1):
+                        if arr[a][b] == standard:
+                            cnt += 1
+    print(cnt)
