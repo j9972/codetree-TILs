dd10a9b2edf22b9c360b9735f9c9161ea3a00eb5
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-27 21:43:44 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 112ms, Memory: 8MB, Status: Passed - CodeTree

--- 231227/괄호 쌍 만들어주기 3/pair-parentheses-3.java
+++ 231227/괄호 쌍 만들어주기 3/pair-parentheses-3.java
@@ -7,24 +7,11 @@ public class Main {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         String s=br.readLine();
         int result=0;
-        Stack<Character> stack = new Stack<>();
 
         for(int i=0; i<s.length(); i++){
             char start=s.charAt(i);
-
-            if(start=='('){
-                stack.push(start);
-            }
-            if(!stack.isEmpty()){
-                if(stack.peek()=='('){
-                    int cnt=0;
-                    for(int j=i+1; j<s.length(); j++){
-                        char end=s.charAt(j);
-                        if(end==')') cnt++;
-                        if(j==s.length()-1) stack.pop();
-                    }
-                    result+=cnt;
-                }
+            for(int j=i+1; j<s.length(); j++){
+                if(start=='(' && s.charAt(j)==')') result++;
             }
         }
         System.out.println(result);
