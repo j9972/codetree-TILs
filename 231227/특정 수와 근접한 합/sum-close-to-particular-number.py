11c8bff309b833068ceb12e1e8a936172a193f85
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-27 22:00:14 +0900

    Title: 특정 수와 근접한 합, Time: 108ms, Memory: 25MB, Status: Passed - CodeTree

--- 231227/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 231227/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,24 +1,14 @@
-import sys
-
 n, s = map(int,input().split())
 values = list(map(int,input().split()))
 
-values.sort()
+res = list()
 
 total = sum(values)
 
-if total <= s:
-    print(s - (total - values[0] - values[1]))
-else:
-    min_val = sys.maxsize
-
-    for i in range(n):
-        for j in range(n):
-
-            if i == j:
-                continue
-            
-            min_val = min(min_val, abs(total - values[i] - values[j]))
-            
-
-    print(abs(min_val - s))
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            continue
+        res.append(abs(s - (total - values[i] - values[j])))
+        
+print(min(res))
