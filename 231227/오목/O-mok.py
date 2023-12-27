a25b5366455dffc0fe93c5f29be133f2bf0436fb
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-27 20:57:26 +0900

    Title: 오목, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231227/오목/O-mok.py
@@ -0,0 +1,57 @@
+arr = [
+    list(map(int,input().split()))
+    for _ in range(19)
+]
+
+dxs = [-1,0,1,1]
+dys = [1,1,1,0]
+
+def in_range(x, y):
+    return 0 <= x < 19 and 0 <= y < 19
+
+ans = 0
+flag = True
+xx,yy=00,00
+
+for x in range(19):
+    for y in range(19):
+
+        if arr[x][y] == 0:
+            continue
+
+        xx,yy=x,y
+
+        for i in range(4):
+            dx, dy = dxs[i], dys[i]
+            curx, cury = x, y
+            val = arr[x][y]
+
+            cnt = 1
+
+            while True:
+                nx ,ny = dx + curx , dy + cury
+
+                if not in_range(nx,ny):
+                    break
+                if arr[nx][ny] != val:
+                    break
+                
+                curx,cury = nx,ny
+                cnt += 1
+            
+            if cnt == 5:
+                ans = val 
+                # print(curx, x)
+                # print(cury, y)
+                xx = (curx + x) // 2 + 1
+                yy = (cury + y) // 2 + 1
+                flag = False
+                break
+        
+        if not flag:
+            break
+    if not flag:
+        break
+
+print(ans)
+print(xx,yy)
