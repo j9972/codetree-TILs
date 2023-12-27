4938cb8ac0580a6354eca86a7ea3f0933a95a942
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-27 21:22:38 +0900

    Title: 최고의 13위치 2, Time: 146ms, Memory: 73MB, Status: Passed - CodeTree

--- 231227/최고의 13위치 2/best-place-of-13-2.py
+++ 231227/최고의 13위치 2/best-place-of-13-2.py
@@ -10,8 +10,13 @@ def in_range(x,y):
 max_cnt = 0
 for i in range(n):
     for j in range(n - 2):
-        for k in range(i + 1, n):
-            for l in range(n - 2):
-                max_cnt = max(max_cnt, arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+        for k in range(n):
+            if i == k:
+                for l in range(j+3,n-2):
+                    max_cnt = max(max_cnt, arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
                                      + arr[k][l] + arr[k][l + 1] + arr[k][l + 2])
+            else:
+                for l in range(n - 2):
+                    max_cnt = max(max_cnt, arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                                        + arr[k][l] + arr[k][l + 1] + arr[k][l + 2])
 print(max_cnt)
