e552e47d032f9779e8c6f84f3214a23cd9eae526
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-28 22:23:55 +0900

    Title: 일렬로 서있는 소 2, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231228/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
+        int result = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                for(int k=j+1; k<n; k++){
+                    if(arr[i] <= arr[j] && arr[j] <= arr[k] && arr[i] <= arr[k]) 
+                        result++;
+                }
+            }
+        }
+        
+        System.out.println(result);
+    }
+}
