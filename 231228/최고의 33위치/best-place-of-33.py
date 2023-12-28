b9e0fbb3b46310adc017a8cc84d94971abf74201
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-28 22:41:01 +0900

    Title: 최고의 33위치, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 231228/최고의 33위치/best-place-of-33.py
+++ 231228/최고의 33위치/best-place-of-33.py
@@ -8,7 +8,9 @@ arr = [
 max_value = 0
 for i in range(n-2):
     for j in range(n-2):
-        sum_value = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j] + arr[i+1][j+1] + arr[i+1][j+2] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2] 
-
+        sum_value = 0
+        for k in range(i,i+3):
+            for h in range(j,j+3):
+                sum_value += arr[k][h]
         max_value = max(max_value, sum_value)
 print(max_value)
