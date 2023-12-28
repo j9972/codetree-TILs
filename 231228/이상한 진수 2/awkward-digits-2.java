c90181ef9f84b6d24848321d9d511d94c77c5d41
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-28 23:31:55 +0900

    Title: 이상한 진수 2, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231228/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String strArr[]= s.split("");
+
+        for(int i=0; i<strArr.length; i++){
+            if(!strArr[i].equals("1")){
+                strArr[i]="1";
+                break;
+            }
+        }
+
+        int z=1;
+        int result=0;
+        for(int i=strArr.length-1; i>=0; i--){
+            result+=Integer.parseInt(strArr[i])*z;
+            z*=2;
+        }
+        System.out.println(result);
+    }
+}
