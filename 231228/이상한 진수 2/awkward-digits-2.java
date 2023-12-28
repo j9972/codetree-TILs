45653c8ab6d6788a2e16b4d67b397788b88b3a0d
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-29 00:00:00 +0900

    Title: 이상한 진수 2, Time: 116ms, Memory: 8MB, Status: Passed - CodeTree

--- 231228/이상한 진수 2/awkward-digits-2.java
+++ 231228/이상한 진수 2/awkward-digits-2.java
@@ -2,34 +2,30 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static String s;
+    public static int max = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        String strArr[]= s.split("");
+        s = br.readLine();
 
-        for(int i=0; i<strArr.length; i++){
-            if(s.length()==1){
-                if(strArr[i].equals("1")){
-                    strArr[i]="0";
-                    break;
-                }else{
-                    strArr[i]="1";
-                    break;
-                }
-            }
-            if(!strArr[i].equals("1")){
-                strArr[i]="1";
-                break;
-            }
-        }
+       for(int i=0; i<s.length(); i++){
+            findBinaryMax(i);
+       }
+        System.out.println(max);
+    }
+    public static void findBinaryMax(int n){
+        int sum=0;
+        int p=0;
 
-        int z=1;
-        int result=0;
-        for(int i=strArr.length-1; i>=0; i--){
-            result+=Integer.parseInt(strArr[i])*z;
-            z*=2;
+        for(int i=s.length()-1; i>=0; i--){
+            if(i==n){
+                sum+=(((s.charAt(i)-'0')+1)%2)*Math.pow(2,p);
+            }else{
+                sum+=(s.charAt(i)-'0')*Math.pow(2,p);
+            }
+            p++;
+            if(sum>max) max=sum;
         }
-        System.out.println(result);
     }
 }
