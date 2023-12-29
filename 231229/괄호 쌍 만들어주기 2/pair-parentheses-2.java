cdbf9bbadebc8a25f00b20a28f6b73ecda91b666
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-29 18:05:30 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

--- 231229/괄호 쌍 만들어주기 2/pair-parentheses-2.java
+++ 231229/괄호 쌍 만들어주기 2/pair-parentheses-2.java
@@ -2,28 +2,19 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        String strArr[] = s.split("");
-        int result = 0;
+        String str = br.readLine();
+        int result=0;
 
-        for (int i = 0; i < s.length() - 3; i++) {
-            for (int j = i + 1; j < s.length() - 2; j++) {
-                String start = strArr[i] + strArr[j];
-                if (start.equals("((")) {
-                    String end = s.substring(j+1,s.length());
-                    if(end.contains("))")){
-                        result++;
-                        break;
-                    }
+        for(int i=0; i<str.length()-1; i++){
+            if(str.charAt(i)=='(' && str.charAt(i+1)=='('){
+                for(int j=i+1; j<str.length()-1; j++){
+                    if(str.charAt(j)==')' && str.charAt(j+1)==')') result++;
                 }
             }
         }
         System.out.println(result);
-
     }
 }
