63aaa4c76548d8d964a3262a8520bf59b7495cda
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-29 21:17:25 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231229/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -0,0 +1,24 @@
+import sys
+n = int(input())
+
+arr = []
+for _ in range(n):
+    x,y = map(int,input().split())
+    arr.append([x,y])
+
+cnt = sys.maxsize
+
+for i in range(n):
+    minx,miny = 40001,40001
+    maxx,maxy = 0,0
+    for j in range(n):
+        if i == j:
+            continue
+        x,y = arr[j]
+        minx = min(minx,x)
+        miny = min(miny,y)
+        maxx = max(maxx,x)
+        maxy = max(maxy,y)
+
+cnt = min(cnt, (maxx - minx) * (maxy - miny))
+print(cnt)
