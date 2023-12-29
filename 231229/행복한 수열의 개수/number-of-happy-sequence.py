1b680ed4ee8e5833be77e71e788814c8fda359bc
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-29 17:19:17 +0900

    Title: 행복한 수열의 개수, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231229/행복한 수열의 개수/number-of-happy-sequence.py
+++ 231229/행복한 수열의 개수/number-of-happy-sequence.py
@@ -6,32 +6,30 @@ arr = [
 
 ans = 0
 
-def cnt_chect(cnt):
-    return cnt == m
-
 def happy(new):
     cnt = 1
-    for a in range(n-m+1): # 2번
-
-        for j in range(a,a+m-1): # 0
-
-            if new[j] != new[j+1]:
-                
-                if cnt_chect(cnt):
-                    return True
+    
+    for i in range(n-m+1): # 2번
+        f = False
+        for j in range(i,i+m-1): # 0
+            
+            if new[j] != new[j+1]:  
+                f = True  
                 break
-
+                
             cnt += 1
         
-        if cnt_chect(cnt):
+        if cnt == m:
             return True
-
+        if f:
+            break
     return False
 
 # 가로
 for i in range(n):
     new = arr[i][:]
     if happy(new):
+        print(new)
         ans += 1
 
 # 세로
