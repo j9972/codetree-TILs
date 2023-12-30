845e2cf1f0eb5f786c1d4ddf3b337100051df752
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-30 19:25:58 +0900

    Title: 숨은 단어 찾기 2, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 231230/숨은 단어 찾기 2/find-hidden-words-2.java
+++ 231230/숨은 단어 찾기 2/find-hidden-words-2.java
@@ -2,10 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static String map[][];
+    public static char map[][];
     public static int n, m;
-    public static int cnt = 0;
-    public static boolean visited[][];
+    public static int result = 0;
     public static int[] dx = new int[]{1, 1, 1, -1, -1, -1, 0, 0};
     public static int[] dy = new int[]{-1, 0, 1, -1, 0, 1, -1, 1};
 
@@ -15,43 +14,34 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        map = new String[n][m];
-        visited = new boolean[n][m];
+        map = new char[n][m];
 
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
-            String strArr[] = s.split("");
             for (int j = 0; j < m; j++) {
-                map[i][j] = strArr[j];
+                map[i][j] = s.charAt(j);
             }
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (map[i][j].equals("L")) {
-                    dfs(i, j, 1);
+                if (map[i][j] == 'L') {
+                    for (int k = 0; k < 8; k++) {
+                        int cnt = 0;
+                        int nx = i;
+                        int ny = j;
+                        for (int l = 0; l < 2; l++) {
+                            nx += dx[k];
+                            ny += dy[k];
+
+                            if (nx < 0 || nx >= n || ny < 0 || ny >= m) break;
+                            if (map[nx][ny] == 'E') cnt++;
+                        }
+                        if (cnt == 2) result++;
+                    }
                 }
             }
         }
-        System.out.println(cnt);
-    }
-
-    public static void dfs(int x, int y, int idx) {
-        visited[x][y] = true;
-
-        if (idx >= 3) {
-            cnt++;
-        }
-        for (int i = 0; i < 8; i++) {
-            int cx = x + dx[i];
-            int cy = y + dy[i];
-
-            if (cx >= 0 && cy >= 0 && cx < n && cy < m) {
-                if (!visited[cx][cy] && map[cx][cy].equals("E")) {
-                    dfs(cx, cy, idx + 1);
-                }
-            }
-        }
+        System.out.println(result);
     }
 }
