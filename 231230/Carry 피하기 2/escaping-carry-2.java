a6de6b1a865a835acd2ac29e172e4a67b2f9d6bf
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-30 17:42:09 +0900

    Title: Carry 피하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231230/Carry 피하기 2/escaping-carry-2.java
+++ 231230/Carry 피하기 2/escaping-carry-2.java
@@ -3,32 +3,67 @@ import java.io.*;
 
 public class Main {
     public static int N;
-    public static int arr[];
-    public static int max = -1;
+    public static int map[][];
+    public static int cnt = 0;
+    public static boolean visited[][];
+    public static int result[][];
+    public static int[] dx = new int[]{1, 1, 1, -1, -1, -1, 0, 0};
+    public static int[] dy = new int[]{-1, 0, 1, -1, 0, 1, -1, 1};
 
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        map = new int[20][20];
+        visited = new boolean[20][20];
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= 19; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= 19; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                for (int k = j + 1; k < N; k++) {
-                    boolean carry = false;
-
-                    if (arr[i] % 10 + arr[j] % 10 + arr[k] % 10 >= 10) carry = true;
-                    if (arr[i] % 100 / 10 + arr[j] % 100 / 10 + arr[k] % 100 / 10 >= 10) carry = true;
-                    if (arr[i] % 1000 / 100 + arr[j] % 1000 / 100 + arr[k] % 1000 / 100 >= 10) carry = true;
-                    if (arr[i] % 10000 / 1000 + arr[j] % 10000 / 1000 + arr[k] % 10000 / 1000 >= 10) carry = true;
-                    if (!carry) max = Math.max(max, arr[i] + arr[j] + arr[k]);
+        int black = 1, white = 2;
+        for (int i = 1; i <= 19; i++) {
+            for (int j = 1; j <= 19; j++) {
+                if (map[i][j] == black && !visited[i][j]) {
+                    cnt = 0;
+                    result = new int[10][2];
+                    dfs(i, j, black,0);
+                } else if (map[i][j] == white && !visited[i][j]) {
+                    cnt = 0;
+                    result = new int[10][2];
+                    dfs(i, j, white,0);
                 }
             }
         }
-        System.out.println(max);
+
+        if (cnt<5) {
+            System.out.println(0);
+        }
+
+    }
+
+    public static void dfs(int x, int y, int color,int idx) {
+        visited[x][y] = true;
+        result[idx][0] = x;
+        result[idx][1] = y;
+        cnt++;
+
+        if (cnt == 5 ) {
+            System.out.println(color);
+            System.out.printf("%d %d", result[2][0], result[2][1]);
+            return;
+        }
+        for (int i = 0; i < 8; i++) {
+            int cx = x + dx[i];
+            int cy = y + dy[i];
+
+            if (cx >= 1 && cy >= 1 && cx < 20 && cy < 20) {
+                if (!visited[cx][cy] && map[cx][cy] == color) {
+                    dfs(cx, cy, color,idx+1);
+                }
+            }
+        }
     }
 }
