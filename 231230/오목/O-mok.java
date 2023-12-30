522553a16d1ef6fb873a770210e3f3d70dfcd548
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-30 17:40:14 +0900

    Title: 오목, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231230/오목/O-mok.java
+++ 231230/오목/O-mok.java
@@ -4,10 +4,11 @@ import java.io.*;
 public class Main {
     public static int N;
     public static int map[][];
-    public static int cnt=0;
+    public static int cnt = 0;
     public static boolean visited[][];
-    public static int dx[] = {0, 0, -1, 1};
-    public static int dy[] = {1, -1, 0, 0};
+    public static int result[][];
+    public static int[] dx = new int[]{1, 1, 1, -1, -1, -1, 0, 0};
+    public static int[] dy = new int[]{-1, 0, 1, -1, 0, 1, -1, 1};
 
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
@@ -22,39 +23,45 @@ public class Main {
             }
         }
 
-        int black=1, white=2;
+        int black = 1, white = 2;
         for (int i = 1; i <= 19; i++) {
             for (int j = 1; j <= 19; j++) {
                 if (map[i][j] == black && !visited[i][j]) {
-                    cnt=0;
-                    dfs(i, j, black);
+                    cnt = 0;
+                    result = new int[10][2];
+                    dfs(i, j, black,0);
                 } else if (map[i][j] == white && !visited[i][j]) {
-                    cnt=0;
-                    dfs(i, j, white);
+                    cnt = 0;
+                    result = new int[10][2];
+                    dfs(i, j, white,0);
                 }
             }
         }
 
-        if(cnt==0){
+        if (cnt == 0) {
             System.out.println(0);
         }
+
     }
 
-    public static void dfs(int x, int y, int color) {
+    public static void dfs(int x, int y, int color,int idx) {
         visited[x][y] = true;
+        result[idx][0] = x;
+        result[idx][1] = y;
         cnt++;
 
-        if(cnt==5){
+        if (cnt == 5 ) {
             System.out.println(color);
-            System.out.printf("%d %d",x,y-2);
+            System.out.printf("%d %d", result[2][0], result[2][1]);
+            return;
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 8; i++) {
             int cx = x + dx[i];
             int cy = y + dy[i];
 
-            if (cx >= 0 && cy >= 0 && cx < 20 && cy < 20) {
+            if (cx >= 1 && cy >= 1 && cx < 20 && cy < 20) {
                 if (!visited[cx][cy] && map[cx][cy] == color) {
-                    dfs(cx, cy, color);
+                    dfs(cx, cy, color,idx+1);
                 }
             }
         }
