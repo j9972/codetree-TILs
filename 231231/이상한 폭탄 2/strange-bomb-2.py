61c253ba19b0b2b6f22512696e5623a80d44141a
정수영 <50689754+j9972@users.noreply.github.com>
2023-12-31 20:02:26 +0900

    Title: 이상한 폭탄 2, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 231231/이상한 폭탄 2/strange-bomb-2.py
+++ 231231/이상한 폭탄 2/strange-bomb-2.py
@@ -5,13 +5,13 @@ boom = [
     for _ in range(n)
 ]
 
-max_val = 0
+max_val = -1
 
 for i in range(n):
     for j in range(n):
         if i == j:
             continue
 
-        if abs(i-j) <= 3 and boom[i] == boom[j]:
+        if abs(i-j) <= k and boom[i] == boom[j]:
             max_val = max(max_val, boom[i])
 print(max_val)
