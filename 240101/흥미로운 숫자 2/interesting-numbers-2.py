266f173355de1331d2d32e17f67fae7b7282032e
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-01 14:52:17 +0900

    Title: 흥미로운 숫자 2, Time: 306ms, Memory: 25MB, Status: Passed - CodeTree

--- 240101/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240101/흥미로운 숫자 2/interesting-numbers-2.py
@@ -2,11 +2,25 @@ x,y = map(int,input().split())
 
 ans = 0
 for i in range(x,y+1):
-
-    res = set()
-    for j in str(i):
-        res.add(j)
+    tmp = dict()
     
-    if len(res) == 2:
+    for j in str(i):
+        if j not in tmp:
+            tmp[j] = 1
+        else:
+            tmp[j] += 1
+
+    if len(tmp) == 1 or len(tmp) >= 3:
+        continue
+
+    #print(tmp, len(tmp))
+
+    cnt = 0
+    for t in tmp:
+        if tmp[t] != 1:
+            cnt += 1
+
+    if cnt == 1:
         ans += 1
+
 print(ans)
