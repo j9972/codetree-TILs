3d39d8630062fdbdedce4e854d82885b429fc944
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-01 16:11:24 +0900

    Title: 요일 맞추기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240101/요일 맞추기/guess-day-of-week.py
+++ 240101/요일 맞추기/guess-day-of-week.py
@@ -1,26 +1,18 @@
 m1, d1, m2, d2 = map(int,input().split())
 
-day = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+def num_of_days(m, d):
+    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days = 0
+    
+    for i in range(1, m):
+        total_days += days[i]
+    total_days += d
+    
+    return total_days    
 
-left_days = 0
-for i in range(m1,m2):
-    left_days += num_of_days[i] 
+diff = num_of_days(m2, d2) - num_of_days(m1, d1)
+while diff < 0:
+    diff += 7
 
-left_days -= d1
-left_days += d2
-
-if left_days % 7 == 0:
-    print(day[0])
-elif left_days % 7 == 1:
-    print(day[1])
-elif left_days % 7 == 2:
-    print(day[2])
-elif left_days % 7 == 3:
-    print(day[3])
-elif left_days % 7 == 4:
-    print(day[4])
-elif left_days % 7 == 5:
-    print(day[5])
-elif left_days % 7 == 6:
-    print(day[6])
+day_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+print(day_of_week[diff % 7])
