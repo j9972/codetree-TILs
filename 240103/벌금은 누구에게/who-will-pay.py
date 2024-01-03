0b9154bef9608f1b0ee6f24fb03f18c31f7bb468
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-03 16:27:11 +0900

    Title: 벌금은 누구에게, Time: 203ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240103/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,14 @@
+n,m,k = map(int,input().split())
+
+arr = [
+    int(input())
+    for _ in range(m)
+]
+
+d = [0] * (n+1)
+
+for i in arr:
+    d[i] += 1
+    if d[i] >= k:
+        print(i)
+        break
