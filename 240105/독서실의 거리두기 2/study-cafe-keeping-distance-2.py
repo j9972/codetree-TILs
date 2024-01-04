5bf8e63c2fa21e9fb23b4275f19be66d03f282f7
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-05 00:15:30 +0900

    Title: 독서실의 거리두기 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240105/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240105/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -4,30 +4,30 @@ n = int(input())
 seat = list(input())
 
 max_dist = 0
-maxX, maxY = -1,-1
+mx, my = -1,-1
 
 for i in range(n):
-    if int(seat[i]) == 1:
+    if seat[i] == '1':
         for j in range(i+1,n):
-            if int(seat[j]) == 1:
-
+            if seat[j] == '1':
                 if j - i > max_dist:
-                    max_dist = abs(j-i)
+                    max_dist = j - i
 
-                    maxX,maxY = i,j
+                    mx,my = i,j
                 break
 
-max_dist2, max_idx = -1,-1
+# 독서실 거리두기3과 다른 점은 양 끝이 1이 아니라 0일 수 있다는 점이다
+max_dist2 = 0
+idx = -1
 if seat[0] == '0':
     dist = 0
     for i in range(1,n):
         if seat[i] == '1':
             break
         dist += 1
-    
     if dist > max_dist2:
         max_dist2 = dist
-        max_idx = 0
+        idx = 0
 
 if seat[n-1] == '0':
     dist = 0
@@ -35,23 +35,19 @@ if seat[n-1] == '0':
         if seat[i] == '1':
             break
         dist += 1
-    
     if dist > max_dist2:
         max_dist2 = dist
-        max_idx = n -1
+        idx = n-1
 
-
-
-if max_dist2 >= max_dist // 2:
-    seats[max_idx] = '1'
+if max_dist // 2 >= max_dist2: 
+    seat[(mx+my)//2] = '1'
 else:
-    seats[(max_i + max_j) // 2] = '1'
-    
-gap = sys.maxsize
+    seat[idx] = '1'
 
+gap = sys.maxsize
 for i in range(n):
-    if int(seat[i]) == 1:
+    if seat[i] == '1':
         for j in range(i+1,n):
-            if int(seat[j]) == 1:
-                gap = min(gap, abs(j-i))
+            if seat[j] == '1':
+                gap = min(gap, j-i)
 print(gap)
