4a19d3ffa9fb7b29d4dd6355a16b15b416f33927
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-04 23:41:28 +0900

    Title: 독서실의 거리두기 3, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240104/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,30 @@
+import sys
+
+n = int(input())
+seat = list(input())
+gap = sys.maxsize
+res = []
+
+print(n, len(seat))
+
+for i in range(n):
+    if seat[i] == '0':
+        # print(i)
+        # seat[i] = '1'
+
+        dist, dist2 = 0, 0
+
+        for j in range(i-1,-1,-1):
+            if seat[j] == '1':
+                dist = abs(i-j)
+                break
+        for j in range(i+1,n):
+            if seat[j] == '1':
+                dist2 = abs(i-j)
+                break
+
+#        print(dist, dist2)
+        res.append(min(dist, dist2))
+    
+        #seat[i] = 0
+print(max(res))
