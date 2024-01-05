a47714a3a80b10340a069879bbb672d06075881e
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-05 22:59:30 +0900

    Title: 홀수 짝수의 묶음, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240105/홀수 짝수의 묶음/odd-even-bundle.py
+++ 240105/홀수 짝수의 묶음/odd-even-bundle.py
@@ -9,20 +9,23 @@ for i in arr:
     else:
         odd += 1
 
-if even > odd:
-    ans = odd * 2 + 1
-elif even == odd:
-    ans = even + odd
-else:
-    ans = even * 2
-
-    size = odd - even
-
-    if (size % 3 == 0):
-        ans += (size//3) * 2
-    else:
-        if((size % 3) % 2 == 0):
-            ans += size // 3 * 2 + 1
+group = 0
+while True:
+    if group % 2 == 0:
+        if even:
+            even -= 1
+            group += 1
+        elif odd >= 2:
+            odd -= 2
+            group += 1
         else:
-            ans += size // 3 * 2 - 1
-print(ans)
+            if even > 0 or odd > 0:
+                group -= 1
+            break
+    else:
+        if odd:
+            odd -= 1
+            group += 1
+        else:
+            break
+print(group)
