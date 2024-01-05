623eec13dbc2293c4d104a7c0f658120f70c8762
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-05 23:31:27 +0900

    Title: 숫자 등장 횟수, Time: 187ms, Memory: 34MB, Status: Passed - CodeTree

--- 240105/숫자 등장 횟수/number-frequency.py
+++ 240105/숫자 등장 횟수/number-frequency.py
@@ -2,14 +2,15 @@ n,m = map(int,input().split())
 n_arr = list(map(int,input().split()))
 m_arr = list(map(int,input().split()))
 
-res = []
-for mm in m_arr:
-    cnt = 0
-    for nn in n_arr:
-        if mm == nn:
-            cnt += 1
-    
-    res.append(cnt)
+dic = dict()
+for i in n_arr:
+    if i in dic:
+        dic[i] += 1
+    else:
+        dic[i] = 1
 
-for i in res:
-    print(i, end=' ')
+for mm in m_arr:
+    if mm in dic:
+        print(dic[mm],end= ' ')
+    else:
+        print(0,end= ' ')
