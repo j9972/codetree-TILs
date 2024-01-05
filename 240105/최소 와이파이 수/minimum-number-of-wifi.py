05835f0844bbaf466a89ca0523d673bd9b822693
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-05 21:11:42 +0900

    Title: 최소 와이파이 수, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240105/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240105/최소 와이파이 수/minimum-number-of-wifi.py
@@ -5,17 +5,10 @@ arr = list(map(int,input().split()))
 cnt = 0
 idx  = 0
 
-while idx <= n:
+while idx < n:
     if arr[idx] == 1:
-        for j in range(idx+1,n):
-            if abs(j-idx) == m:
-                cnt += 1
-                idx += j + 1
-                break
-            if j == n-1:
-                cnt += 1
-                break
-        #print(idx , cnt)
+        cnt += 1
+        idx +=  (2*m) + 1
     else:
         idx += 1
     
