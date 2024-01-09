d682355e6728f3dcd34b425534fd58b7a1d0bbc1
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-09 21:41:22 +0900

    Title: 정수 찾기, Time: 184ms, Memory: 36MB, Status: Passed - CodeTree

--- 240109/정수 찾기/find-an-integer.py
+++ 240109/정수 찾기/find-an-integer.py
@@ -1,11 +1,10 @@
 a = int(input())
-a_list = list(map(int,input().split()))
-
+a_list = set(list(map(int,input().split())))
 b = int(input())
 b_set = list(map(int,input().split()))
 
 for val_b in b_set:
-    if val_b in set(a_list):
-        print(1)
-    else:
+    if val_b not in a_list:
         print(0)
+    else:
+        print(1)
