7d37abf45b505ad088e17829a88d45a55307f857
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-09 21:08:31 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 450ms, Memory: 26MB, Status: Passed - CodeTree

--- 240109/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
+++ 240109/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
@@ -1,25 +1,46 @@
 from collections import defaultdict
 
 n = int(input())
-res = []
+# res = []
+
+# for _ in range(n):
+#     val = input()
+
+#     dic = defaultdict(int)
+
+#     for i in val:
+#         if i in dic:
+#             dic[i] += 1
+#         else:
+#             dic[i] = 1
+#     res.append(dic)
+
+# max_val = 0
+# for i in range(len(res)):
+#     val = 1
+#     for j in range(i+1,len(res)):
+#         if res[i] == res[j]:
+#             val += 1
+#     max_val = max(max_val, val)
+# print(max_val)
+
+dic = {}
 
 for _ in range(n):
     val = input()
+    val_sorted = sorted(val)
 
-    dic = defaultdict(int)
+    string = ""
+    for e in val_sorted:
+        string += e
+    
+    if string in dic:
+        dic[string] += 1
+    else:
+        dic[string] = 1
 
-    for i in val:
-        if i in dic:
-            dic[i] += 1
-        else:
-            dic[i] = 1
-    res.append(dic)
-
-max_val = 0
-for i in range(len(res)):
-    val = 1
-    for j in range(i+1,len(res)):
-        if res[i] == res[j]:
-            val += 1
-    max_val = max(max_val, val)
-print(max_val)
+res = 0
+for k,v in dic.items():
+    if res < v:
+        res = v
+print(res)
