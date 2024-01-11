c8b7e3a5ac988f7f990064d247b5c6e75924ced3
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-11 21:20:52 +0900

    Title: C-TAG, Time: 133ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240111/C-TAG/c-tag.py
@@ -0,0 +1,38 @@
+from itertools import combinations as cb
+
+n,m = map(int,input().split())
+
+a = [
+    input()
+    for _ in range(n)
+]
+
+b = [
+    input()
+    for _ in range(n)
+]
+
+numList = list(range(0,8))
+
+ans = 0
+for num in cb(numList,3):
+    aset = set()
+    bset = set()
+
+    for i in range(n):
+        string1 = ""
+        string2 = ""
+
+        for k in num:
+            string1 += a[i][k]
+            string2 += b[i][k]
+        
+        aset.add(string1)
+        bset.add(string2)
+    
+    alen = len(aset)
+    blen = len(bset)    
+
+    if len(aset | bset) == alen + blen:
+        ans += 1
+print(ans)
