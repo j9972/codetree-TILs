13355175f868768131392e9b134d3afae04c5011
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-11 23:30:04 +0900

    Title: 대칭 차집합, Time: 325ms, Memory: 62MB, Status: Passed - CodeTree

--- 240111/대칭 차집합/symmetric-difference-set.py
+++ 240111/대칭 차집합/symmetric-difference-set.py
@@ -1,16 +1,14 @@
 a,b = map(int,input().split())
 
-a_list = list(map(int,input().split()))
-b_list = list(map(int,input().split()))
+a_list = set(map(int,input().split()))
+b_list = set(map(int,input().split()))
 
-a_set = set()
-b_set = set()
-
+ans = 0
 for i in a_list:
     if i not in b_list:
-        a_set.add(i)
+        ans += 1
 for i in b_list:
     if i not in a_list:
-        b_set.add(i)
+        ans += 1
 
-print(len(a_set) + len(b_set))
+print(ans)
