22b911fc0cdc799043633d7be782d9ea4801e9e1
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-12 01:27:01 +0900

    Title: 정수 명령 처리 6, Time: 363ms, Memory: 29MB, Status: Passed - CodeTree

--- 240112/정수 명령 처리 6/process-numeric-commands-6.py
+++ 240112/정수 명령 처리 6/process-numeric-commands-6.py
@@ -1,26 +1,45 @@
 import heapq
 
+class PriorityQueue:
+    def __init__(self):          # 빈 Priority Queue 하나를 생성합니다.
+        self.items = []
+                
+    def push(self, item):        # 우선순위 큐에 데이터를 추가합니다.
+        heapq.heappush(self.items, -item)
+                
+    def empty(self):             # 우선순위 큐가 비어있으면 True를 반환합니다.
+        return not self.items
+                
+    def size(self):              # 우선순위 큐에 있는 데이터 수를 반환합니다.
+        return len(self.items)
+        
+    def pop(self):               # 우선순위 큐에 있는 데이터 중 최댓값에 해당하는 데이터를 반환하고 제거합니다.
+        if self.empty():
+            raise Exception("PriorityQueue is empty")
+            
+        return -heapq.heappop(self.items)
+                
+    def top(self):               # 우선순위 큐에 있는 데이터 중 최댓값에 해당하는 데이터를 제거하지 않고 반환합니다.
+        if self.empty():
+            raise Exception("PriorityQueue is empty")
+                        
+        return -self.items[0]
+
+
+# 변수 선언 및 입력:
 n = int(input())
-heap = []
+pq = PriorityQueue()
 
 for _ in range(n):
-    data = list(input().split())
-    heapq.heapify(heap)
-
-    if data[0] == 'push':
-        heapq.heappush(heap, -int(data[1]))
-
-    elif data[0] == 'size':
-        print(len(heap))
-
-    elif data[0] == 'empty':
-        if len(heap) == 0:
-            print(1)
-        else:
-            print(0)
-
-    elif data[0] == 'pop':
-        print(-heapq.heappop(heap))
-
-    elif data[0] == 'top':
-        print(heap[-1])
+    command = input()
+    if command.startswith("push"):
+        x = int(command.split()[1])
+        pq.push(x)
+    elif command == "pop":
+        print(pq.pop())
+    elif command == "size":
+        print(pq.size())
+    elif command == "empty":
+        print(1 if pq.empty() else 0)
+    else:
+        print(pq.top())
