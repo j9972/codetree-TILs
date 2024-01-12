04ff7002aea30853a82c7095e675b6025afeb39a
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-12 22:20:44 +0900

    Title: 마지막으로 남은 숫자, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240112/마지막으로 남은 숫자/last-remaining-number.py
+++ 240112/마지막으로 남은 숫자/last-remaining-number.py
@@ -8,9 +8,12 @@ heap = []
 for i in arr:
     heapq.heappush(heap, -i)
 
-while len(heap) != 1:
-    val1 = heapq.heappop(heap)
-    val2 = heapq.heappop(heap)
+while True:
+    if not heap or len(heap) == 1:
+        break
+
+    val1 = -heapq.heappop(heap)
+    val2 = -heapq.heappop(heap)
 
     if abs(val1-val2) == 0:
         continue
