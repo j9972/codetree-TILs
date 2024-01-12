f074c0eda75213b74720ea56846ac18c95621799
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-12 22:19:04 +0900

    Title: 마지막으로 남은 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240112/마지막으로 남은 숫자/last-remaining-number.py
@@ -0,0 +1,23 @@
+import heapq
+
+n = int(input())
+
+arr = list(map(int,input().split()))
+
+heap = []
+for i in arr:
+    heapq.heappush(heap, -i)
+
+while len(heap) != 1:
+    val1 = -heapq.heappop(heap)
+    val2 = -heapq.heappop(heap)
+
+    if abs(val1-val2) == 0:
+        continue
+    else:
+        heapq.heappush(heap, -abs(val1-val2))
+
+if not heap:
+    print(0)
+else:
+    print(-heapq.heappop(heap))
