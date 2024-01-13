51c6eb13a88eea9e0e274e43bafb246357f6a84f
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-13 22:37:28 +0900

    Title: k번째로 작은 쌍의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240113/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
+++ 240113/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -10,16 +10,14 @@ heap = [] # 2개의 원소와 그 합을 넣기
 a.sort()
 b.sort()
 
-for i in range(n):
-    heapq.heappush(heap, (a[i] + b[0], i, 0))
+for i in range(m):
+    heapq.heappush(heap, (a[0] + b[i], 0, i))
 
 for i in range(k-1):
     _, idx1, idx2 = heapq.heappop(heap)
 
-    #print(idx1, idx2)
-
-    idx2 += 1 
-    if idx2 <= m:
+    idx1 += 1 
+    if idx1 <= n:
         heapq.heappush(heap, (a[idx1] + b[idx2], idx1, idx2))
 
 print(heapq.heappop(heap)[0])
