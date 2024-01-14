861f831ec79e3b525512cf7a811a9e9fad8e4c61
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-14 20:26:37 +0900

    Title: 겹치지 않게 선분 고르기, Time: 225ms, Memory: 28MB, Status: Passed - CodeTree

--- 240114/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240114/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -5,19 +5,49 @@ arr = [
     for _ in range(n)
 ]
 
-d = [0] * 1001
-cnt = 0
+ans = 0
+select = list()
 
-for i in arr:
-    x1,x2 = i
-    flag = False
+def possible():
+    for idx1, val1 in enumerate(select):
+        for idx2, val2 in enumerate(select):
+            x1,y1 = val1
+            x2,y2 = val2
+
+            if idx1 < idx2 and ((x1<=x2<=y1) or (x1<=y2<=y1) or (x2<=x1<=y2) or (x2<=y1<=y2)):
+                return False
+    return True
+
+def choose(cnt, num):
+    global ans
+    if cnt > n or num > n:
+        return
     
-    for j in range(x1,x2+1):
-        if d[j] == 1:
-            flag = True
-            break
-        d[j] = 1
+    if possible():
+        ans = max(ans, len(select))
+    
+    for i in range(num,n):
+        select.append(arr[i])
+        choose(cnt+1, i+1)
+        select.pop()
+        
+choose(0,0)
+print(ans)
 
-    if flag == False:
-        cnt += 1
-print(cnt)
+# arr.sort()
+
+# d = [0] * 1001
+# cnt = 0
+
+# for x1,x2 in arr:
+#     flag = False
+    
+#     for j in range(x1,x2+1):
+#         if d[j] == 1:
+#             flag = True
+#             break
+#         d[j] = 1
+
+#     if flag == False:
+#         cnt += 1
+# print(cnt)
