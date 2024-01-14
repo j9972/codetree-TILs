f9d3c12b97a97153556d6207a54b676bdf5be75b
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-15 01:03:27 +0900

    Title: 코드네임, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240115/코드네임/code-name.py
+++ 240115/코드네임/code-name.py
@@ -1,12 +1,32 @@
-students = []
-for i in range(5):
-    a,b = input().split()
-    students.append((a,int(b)))
+# students = []
+# for i in range(5):
+#     a,b = input().split()
+#     students.append((a,int(b)))
 
-idx = 0
-min_val = students[0][1]
+# idx = 0
+# min_val = students[0][1]
+# for i in range(1,5):
+#     if students[i][1] < min_val:
+#         min_val = students[i][1]
+#         idx = i
+# print(students[idx][0], students[idx][1])
+
+class User:
+    def __init__(self, code_name, score):
+        self.code_name = code_name
+        self.score = score
+
+
+# 변수 선언 및 입력
+users = []
+
+for _ in range(5):
+    code_name, score = tuple(input().split())
+    users.append(User(code_name, int(score)))
+
+min_idx = 0
 for i in range(1,5):
-    if students[i][1] < min_val:
-        min_val = students[i][1]
-        idx = i
-print(students[idx][0], students[idx][1])
+    if users[min_idx].score > users[i].score:
+        min_idx = i
+
+print(users[min_idx].code_name, users[min_idx].score)
