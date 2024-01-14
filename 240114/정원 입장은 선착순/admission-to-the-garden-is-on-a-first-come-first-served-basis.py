53d1008eb15bd4b4804300358fdb504af0f4b525
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-14 12:05:58 +0900

    Title: 정원 입장은 선착순, Time: 665ms, Memory: 37MB, Status: Passed - CodeTree

---
+++ 240114/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -0,0 +1,31 @@
+import heapq
+import sys
+
+n = int(input())
+
+arr = []
+for i in range(1,n+1):
+    a,t = tuple(map(int,input().split()))
+    arr.append((a, i, t))
+
+arr.append((sys.maxsize, n+1, 0))
+
+arr.sort()
+
+waiting = []
+ans, exit_time = 0, 0
+
+for a , idx, t in arr:
+    while a >= exit_time and waiting:
+        _, next_a, next_t = heapq.heappop(waiting)
+
+        ans = max(ans, exit_time - next_a)
+
+        exit_time += next_t
+
+
+    if a >= exit_time:
+        exit_time = a + t
+    else:
+        heapq.heappush(waiting, (idx, a, t))
+print(ans)
