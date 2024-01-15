67af7980eb3516b18476819d40e4b1460b428685
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-15 12:37:16 +0900

    Title: 원점으로부터의 거리, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240115/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,10 @@
+n = int(input())
+info = list()
+for i in range(n):
+    x,y = map(int,input().split())
+    info.append((x,y,i+1))
+
+
+info.sort(key = lambda x : (abs(x[0] - 0) + abs(x[1] - 0), x[2] ))
+for i in info:
+    print(i[2])
