6eb6943e8d62b5dfd47aeeb72a2b5bdf26c9a919
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-16 14:53:18 +0900

    Title: xor 결과 최대 만들기, Time: 390ms, Memory: 29MB, Status: Passed - CodeTree

--- 240116/xor 결과 최대 만들기/max-of-xor.py
+++ 240116/xor 결과 최대 만들기/max-of-xor.py
@@ -3,40 +3,30 @@ n,m = map(int,input().split())
 arr = list(map(int,input().split()))
 
 ans = []
-max_val = 0
 visited = [False] * (n+1)
 
-def make_binary(val):
-    binary_val = format(val,'b')
-    return binary_val
-
-def Print():
-    tmp_val = ans[0]
-    if len(ans) == 1:
-        return ans[0]
-    else:
-        for i in range(1,m):
-            tmp_val = tmp_val ^ ans[i]
-    
-        return tmp_val
-    
-
-def choose(cur):
-    global max_val
+def choose(cur, idx):
     res = 0
-    if cur == m:
-        #print("Print() : ",Print())
-        max_val = max(max_val, Print())
-        return
+    if cur == m+1:
+        res = ans[0]
+        for i in range(1,m):
+            res = res ^ ans[i]
+        return res
 
-    for i in range(n):
-        if not visited[i]:
-            visited[i] = True
-            ans.append(arr[i])
+    max_val = 0
+    for i in range(idx+1, n):
+        ans.append(arr[i])
+        max_val = max(max_val, choose(cur+1, i))
+        ans.pop()
+    return max_val
+    # for i in range(n):
+    #     if not visited[i]:
+    #         visited[i] = True
+    #         ans.append(arr[i])
             
-            choose(cur+1)
+    #         max_val = max(max_val, choose(cur+1))
             
-            ans.pop()
-            visited[i] = False
-choose(0)
-print(max_val)
+    #         ans.pop()
+    #         visited[i] = False
+
+print(choose(1,-1))
