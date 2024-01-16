d04d0c4330d4b851387808c3d925ed950f79cd16
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-16 14:32:52 +0900

    Title: xor 결과 최대 만들기, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240116/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,39 @@
+n,m = map(int,input().split())
+
+arr = list(map(int,input().split()))
+
+ans = []
+max_val = 0
+visited = [False] * (n+1)
+
+def make_binary(val):
+    binary_val = format(val,'b')
+    return binary_val
+
+def Print():
+    tmp_val = ans[0]
+    for i in range(1,m):
+        tmp_val = tmp_val ^ ans[i]
+    
+    return tmp_val
+    
+
+def choose(cur):
+    global max_val
+    res = 0
+    if cur == m:
+        max_val = max(max_val, Print())
+        #Print()
+        return
+
+    for i in range(1,n+1):
+        if not visited[i]:
+            visited[i] = True
+            ans.append(i)
+            
+            choose(cur+1)
+            
+            ans.pop()
+            visited[i] = False
+choose(0)
+print(max_val)
