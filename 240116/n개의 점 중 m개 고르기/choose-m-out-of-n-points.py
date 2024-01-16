2a48d0004adacbb29e12e285442d0629d72a264b
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-16 16:13:03 +0900

    Title: n개의 점 중 m개 고르기, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240116/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240116/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -1,27 +1,33 @@
 import sys
-from itertools import combinations as cb
 n,m = map(int,input().split())
 
-min_val = sys.maxsize
-
-arr = [
+arr =[
     tuple(map(int,input().split()))
     for _ in range(n)
 ]
 
+ans = []
+min_val = sys.maxsize
+
 def find_dist(a,b,c,d):
-    return (abs(c-a) ** 2) + (abs(d-b) ** 2)
+    return (c-a) ** 2 + (d-b) ** 2
 
-possibleCB = list(cb(arr,m))
+def choose(idx, cnt):
+    global min_val    
+    if cnt == m:
+        for i, val1 in enumerate(ans):
+            for j, val2 in enumerate(ans):
+                if i != j:
+                    min_val = min(min_val, find_dist(val1[0], val1[1], val2[0], val2[1]))
+                    return
+    if idx == n:
+        return
 
-for possible in possibleCB:
-    max_val = -sys.maxsize
-    two = list(cb(possible,2))
+    ans.append(arr[idx])
+    choose(idx+1,cnt+1)
+    ans.pop()
 
-    for i in two:
-        distance = find_dist(i[0][0], i[0][1] , i[1][0], i[1][1])
-        max_val = max(max_val, distance)
+    choose(idx+1, cnt)
 
-    min_val = min(max_val, min_val)
-
+choose(0,0)
 print(min_val)
