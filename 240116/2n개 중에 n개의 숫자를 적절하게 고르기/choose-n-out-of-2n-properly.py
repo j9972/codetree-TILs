ceaee5ed79265d70d056e82f37b1f0e98ab9c67a
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-16 16:48:46 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 171ms, Memory: 27MB, Status: Passed - CodeTree

--- 240116/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ 240116/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -1,42 +1,48 @@
-# import sys
-
-# n = int(input())
-# arr = list(map(int,input().split()))
-
-# ans = []
-# total = sum(arr)
-# visited = [False] * (2 * n + 1)
-
-# min_val = sys.maxsize
-
-# def choose(cnt):
-#     global min_val
-
-#     if cnt == n:
-#         min_val = min(min_val, abs((total - sum(ans)) - sum(ans)))
-#         return
-    
-#     for i in range(2*n):
-#         if not visited[i]:
-#             ans.append(arr[i])
-#             visited[i] = True
-
-#             choose(cnt+1)
-
-#             ans.pop()
-#             visited[i] = False
-# choose(0)
-# print(min_val)
 import sys
-from itertools import combinations as cb
 
 n = int(input())
 arr = list(map(int,input().split()))
 
-tmp = list(cb(arr,n))
+visited = [False] * (2 * n)
 
 min_val = sys.maxsize
 
-for i in tmp:
-    min_val = min(min_val, abs((sum(arr) - sum(i)) - sum(i)))
+def calc():
+    diff = 0
+    for i in range(2*n):
+        if visited[i]:
+            diff += arr[i]
+        else:
+            diff -= arr[i]
+    return abs(diff)
+
+def choose(idx, cnt):
+    global min_val
+
+    if cnt == n:
+        min_val = min(min_val, calc())
+        return
+    if idx == 2*n:
+        return
+    
+    visited[idx] = True
+
+    choose(idx+1,cnt+1)
+    visited[idx] = False
+
+    choose(idx+1, cnt)
+choose(0,0)
 print(min_val)
+# import sys
+# from itertools import combinations as cb
+
+# n = int(input())
+# arr = list(map(int,input().split()))
+
+# tmp = list(cb(arr,n))
+
+# min_val = sys.maxsize
+
+# for i in tmp:
+#     min_val = min(min_val, abs((sum(arr) - sum(i)) - sum(i)))
+# print(min_val)
