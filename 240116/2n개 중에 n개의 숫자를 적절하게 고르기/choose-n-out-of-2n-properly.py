2a02abd31b527e1f1f7a9401fbcf44908635af3d
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-16 16:45:42 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 196ms, Memory: 25MB, Status: Passed - CodeTree

--- 240116/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ 240116/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -1,27 +1,42 @@
+# import sys
+
+# n = int(input())
+# arr = list(map(int,input().split()))
+
+# ans = []
+# total = sum(arr)
+# visited = [False] * (2 * n + 1)
+
+# min_val = sys.maxsize
+
+# def choose(cnt):
+#     global min_val
+
+#     if cnt == n:
+#         min_val = min(min_val, abs((total - sum(ans)) - sum(ans)))
+#         return
+    
+#     for i in range(2*n):
+#         if not visited[i]:
+#             ans.append(arr[i])
+#             visited[i] = True
+
+#             choose(cnt+1)
+
+#             ans.pop()
+#             visited[i] = False
+# choose(0)
+# print(min_val)
 import sys
+from itertools import combinations as cb
 
 n = int(input())
 arr = list(map(int,input().split()))
 
-ans = []
-total = sum(arr)
-visited = [False] * (2 * n+1)
+tmp = list(cb(arr,n))
 
 min_val = sys.maxsize
 
-def choose(cnt):
-    global min_val
-
-    if cnt == n:
-        min_val = min(min_val, abs((total - sum(ans)) - sum(ans)))
-        return
-    
-    for i in range(2*n):
-        if not visited[i]:
-            ans.append(arr[i])
-            visited[i] = True
-            choose(cnt+1)
-            ans.pop()
-            visited[i] = False
-choose(0)
+for i in tmp:
+    min_val = min(min_val, abs((sum(arr) - sum(i)) - sum(i)))
 print(min_val)
