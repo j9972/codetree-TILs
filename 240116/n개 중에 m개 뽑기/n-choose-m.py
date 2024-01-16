c5d2bc833f183b473ea54803c9a506e2a033807a
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-16 13:14:57 +0900

    Title: n개 중에 m개 뽑기, Time: 151ms, Memory: 26MB, Status: Passed - CodeTree

--- 240116/n개 중에 m개 뽑기/n-choose-m.py
+++ 240116/n개 중에 m개 뽑기/n-choose-m.py
@@ -7,14 +7,33 @@ def Print():
         print(e, end=' ')
     print()
 
-def choose(cur):
-    if cur == m+1:
-        Print()
+# def choose(cur):
+#     if cur == m+1:
+#         Print()
+#         return
+    
+#     for i in range(1,n+1):
+#         if len(ans) == 0 or ans[-1] < i:
+#             ans.append(i)
+#             choose(cur + 1)
+#             ans.pop()
+# choose(1)
+
+def choose(cur,cnt):
+    if cur == n+1:
+        if cnt == m:
+            Print()
         return
     
-    for i in range(1,n+1):
-        if len(ans) == 0 or ans[-1] < i:
-            ans.append(i)
-            choose(cur + 1)
-            ans.pop()
-choose(1)
+    # for i in range(1,n+1):
+    #     if len(ans) == 0 or ans[-1] < i:
+    #         ans.append(i)
+    #         choose(cur + 1)
+    #         ans.pop()
+
+    ans.append(cur)
+    choose(cur + 1, cnt + 1)
+    ans.pop()
+    choose(cur + 1, cnt)
+
+choose(1,0)
