b9b1d1a29a75461e2755b8d569539f991479def3
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-16 16:16:51 +0900

    Title: n개의 점 중 m개 고르기, Time: 210ms, Memory: 27MB, Status: Passed - CodeTree

--- 240116/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240116/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -12,14 +12,21 @@ min_val = sys.maxsize
 def find_dist(a,b,c,d):
     return (c-a) ** 2 + (d-b) ** 2
 
+def calc():
+    max_val = -sys.maxsize
+    for i, val1 in enumerate(ans):
+        for j, val2 in enumerate(ans):
+            if i != j:
+                distance = find_dist(val1[0], val1[1], val2[0], val2[1])
+                max_val = max(max_val, distance)
+    return max_val
+
 def choose(idx, cnt):
     global min_val    
     if cnt == m:
-        for i, val1 in enumerate(ans):
-            for j, val2 in enumerate(ans):
-                if i != j:
-                    min_val = min(min_val, find_dist(val1[0], val1[1], val2[0], val2[1]))
-                    return
+        min_val = min(min_val , calc())
+        return
+
     if idx == n:
         return
 
