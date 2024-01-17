add521cad70e31c379feba036b34c59492f9d61c
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-17 20:21:04 +0900

    Title: 만나는 그 순간, Time: 193ms, Memory: 40MB, Status: Passed - CodeTree

--- 240117/만나는 그 순간/the-moment-we-meet.py
+++ 240117/만나는 그 순간/the-moment-we-meet.py
@@ -1,40 +1,37 @@
 n,m = map(int,input().split())
 
-DA = [0] * 1001
-DB = [0] * 1001
+DA = [0] * 1000001
+DB = [0] * 1000001
 
 a_idx = 1
 for i in range(n):
     d,t = input().split()
     t = int(t)
 
-    for j in range(a_idx, a_idx+t):
+    for _ in range(t):
         if d == 'R':
-            DA[j] = DA[j-1] + 1
+            DA[a_idx] = DA[a_idx-1] + 1
         else:
-            DA[j] = DA[j-1] - 1
-    a_idx += t
+            DA[a_idx] = DA[a_idx-1] - 1
+        a_idx += 1
+
 
 b_idx = 1
 for i in range(m):
     d,t = input().split()
     t = int(t)
 
-    for j in range(b_idx, b_idx+t):
+    for _ in range(t):
         if d == 'R':
-            DB[j] = DB[j-1] + 1
+            DB[b_idx] = DB[b_idx-1] + 1
         else:
-            DB[j] = DB[j-1] - 1
-    b_idx += t
-
-for i in range(a_idx,max(a_idx,b_idx)):
-    DA[i] = DA[a_idx-1]
-for i in range(b_idx,max(a_idx,b_idx)):
-    DB[i] = DB[b_idx-1]
+            DB[b_idx] = DB[b_idx-1] - 1
+        b_idx += 1
 
 ans = -1
-for i in range(1,max(a_idx,b_idx)):
+for i in range(1,a_idx):
     if DA[i] == DB[i]:
         ans = i
         break
+
 print(ans)
