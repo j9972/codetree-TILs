d48ccf6b676da2ac452827c57e7fabf0a125cf20
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-18 13:28:39 +0900

    Title: 벌금은 누구에게, Time: 134ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240118/벌금은 누구에게/who-will-pay.py
+++ 240118/벌금은 누구에게/who-will-pay.py
@@ -1,17 +1,15 @@
 n,m,k = map(int,input().split())
 
-d = [1] * 101
+d = [0] * 101
 for i in range(m):
     d[int(input())] += 1
 
 #print(d)
 
 ans = -1
-for i in d:
-    if i >= k:
-        ans += 1
+for i in range(101):
+    if d[i] >= k:
+        ans = i
+        break
 
-if ans != -1:
-    print(ans + 1)
-else:
-    print(ans)
+print(ans)
