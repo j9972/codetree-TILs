604b0613e4c168147159dd16425d0a0dc1ee9a6c
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-18 15:27:57 +0900

    Title: 연속된 숫자 만들기 2, Time: 239ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240118/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -3,7 +3,7 @@ a.sort()
 
 if a[0] + 1 == a[1] and a[1] + 1 == a[2]:
     print(0)
-if a[0] + 2 == a[1] or a[1] + 2 == a[2]:
+elif a[0] + 2 == a[1] or a[1] + 2 == a[2]:
     print(1)
 else:
     print(2)
