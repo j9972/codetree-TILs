67e2cb3923f6ad9c391027880b3263a4ac5a55c8
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-18 13:51:23 +0900

    Title: 좌우로 움직이는 로봇, Time: 131ms, Memory: 40MB, Status: Wrong Answer - CodeTree

---
+++ 240118/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,42 @@
+n,m = map(int,input().split())
+
+A = [0] * 1000001
+B = [0] * 1000001
+
+idx = 0
+for i in range(n):
+    t,d = input().split()
+    t = int(t)
+
+    for _ in range(t):
+        if d == 'R':
+            A[idx] = A[idx-1] + 1
+        elif d == 'L':
+            A[idx] = A[idx-1] - 1
+        idx += 1
+
+b_idx = 0
+for i in range(m):
+    t,d = input().split()
+    t = int(t)
+
+    for _ in range(t):
+        if d == 'R':
+            B[b_idx] = B[b_idx-1] + 1
+        elif d == 'L':
+            B[b_idx] = B[b_idx-1] - 1
+        b_idx += 1
+
+if idx > b_idx:
+    for i in range(b_idx,idx+1):
+        B[i] = B[b_idx]
+elif idx < b_idx:
+    for i in range(idx,b_idx+1):
+        A[i] = A[idx]
+
+ans = 0
+for i in range(1,max(idx,b_idx)+1):
+    if A[i-1] != B[i-1]:
+        if A[i] == B[i]:
+            ans += 1
+print(ans)
