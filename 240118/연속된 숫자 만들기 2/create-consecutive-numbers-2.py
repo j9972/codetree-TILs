3fa8837e05ff3a3b0f14c8b508f5f1f185665651
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-18 15:27:27 +0900

    Title: 연속된 숫자 만들기 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240118/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240118/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,4 +1,9 @@
 a = list(map(int,input().split()))
 a.sort()
 
-print(min(a[1] - a[0], a[2] - a[1]) - 1)
+if a[0] + 1 == a[1] and a[1] + 1 == a[2]:
+    print(0)
+if a[0] + 2 == a[1] or a[1] + 2 == a[2]:
+    print(1)
+else:
+    print(2)
