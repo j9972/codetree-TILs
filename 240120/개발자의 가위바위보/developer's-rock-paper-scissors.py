32329fd33b68cd78905a4154ea830558cdd6554c
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-20 15:52:13 +0900

    Title: 개발자의 가위바위보, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240120/개발자의 가위바위보/developer's-rock-paper-scissors.py
+++ 240120/개발자의 가위바위보/developer's-rock-paper-scissors.py
@@ -5,13 +5,27 @@ arr = [
     for _ in range(n)
 ]
 
-one, two = 0,0
+max_win , win = 0,0
 
 for a,b in arr:
-    if a == b:
-        continue
-    if abs(a-b) == 1:
-        one += 1
-    else:
-        two += 1
-print(max(one, two))
+    
+    if a == 1 and b == 2:
+        win += 1
+    elif a == 2 and b == 3:
+        win += 1
+    elif a == 3 and b == 1:
+        win += 1
+max_win = max(max_win, win)
+
+win = 0
+for a,b in arr:
+    
+    if a == 1 and b == 3:
+        win += 1
+    elif a == 2 and b == 1:
+        win += 1
+    elif a == 3 and b == 2:
+        win += 1
+max_win = max(max_win, win)
+
+print(max_win)
