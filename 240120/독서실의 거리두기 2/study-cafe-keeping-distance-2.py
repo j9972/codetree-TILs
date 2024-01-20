b5a88105777ec5b17884770c77f05a2b1f266870
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-20 17:17:35 +0900

    Title: 독서실의 거리두기 2, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240120/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240120/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -1,42 +1,53 @@
-n = int(input())
-arr = input()
+import sys
 
+n = int(input()) 
+arr = list(input())
 
-cur_x ,cur_y= -1, -1
-
-for i in range(n):
-    dist = 0
-    for j in range(i+1,n):
-        if i == j:
-            continue
-
-        if arr[j] == '1' or j == n-1:
-            if dist < j-i:
-                dist = j - i
-                cur_x ,cur_y = i,j
-                break
-        
-print(cur_x ,cur_y)
-#arr[cur_idx] = '1'
-
-max_dist = list()
+max_dist = 0
+cur_i,cur_j = -1,-1
 
 for i in range(n):
     for j in range(i+1,n):
-        if i == j:
-            continue
-
-        if arr[i] == '1' and arr[j] == '1':
-            max_dist.append(j-i)
+        if arr[i] == '1' and arr[j] =='1':
+            if max_dist < j - i:
+                max_dist = j - i
+                cur_i, cur_j = i,j
             break
 
-print(max_dist)
-max_dist.sort()
+max_dist2 , max_idx = -1,-1
 
-max_dist_data = max_dist.pop()
+if arr[0] == '0':
+    dist = 0
+    for i in range(1,n):
+        if arr[i] == '1':
+            break
+        dist += 1
+    
+    if dist > max_dist2:
+        max_dist2 = dist
+        max_idx = 0
 
-max_dist.append(max_dist_data // 2)
+if arr[-1] == '0':
+    dist = 0
+    for i in range(n-2,-1,-1):
+        if arr[i] == '1':
+            break
+        dist += 1
+    
+    if dist > max_dist2:
+        max_dist2 = dist
+        max_idx = n-1
 
-max_dist.sort()
+if max_dist2 >= max_dist // 2:
+    arr[max_idx] = '1'
+else:
+    arr[(cur_i + cur_j) // 2] = '1'
 
-print(max_dist[0])
+
+ans = sys.maxsize
+for i in range(n):
+    for j in range(i+1,n):
+        if arr[i] == '1' and arr[j] =='1':
+            ans = min(ans, j-i)
+            break
+print(ans)
