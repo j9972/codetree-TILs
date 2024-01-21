cc340d693e65c24fa743fcb14e7f2170e3c12fb3
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-21 15:20:31 +0900

    Title: 외판원 순회, Time: 444ms, Memory: 26MB, Status: Passed - CodeTree

--- 240121/외판원 순회/traveling-salesman-problem.py
+++ 240121/외판원 순회/traveling-salesman-problem.py
@@ -7,28 +7,41 @@ arr = [
 ]
 
 min_val = sys.maxsize
-visited = [False] * (n+1)
+visited = [False] * n
 ans = []
 
 def choose(cur_idx):
     global min_val
 
     if cur_idx == n:
-        min_val = min(min_val, sum(ans))
+        tot = 0
+        for i in range(n-1):
+            cost = arr[ans[i]][ans[i+1]]
+
+            if cost == 0:
+                return
+            tot += cost
+        
+        addition = arr[ans[-1]][0]
+        if addition == 0:
+            return
+        
+        min_val = min(min_val, tot + addition)
         return
     
     for i in range(n):
-
-        if arr[cur_idx][i] == 0:
-            continue
-
         if not visited[i]:
             visited[i] = True
-            ans.append(arr[cur_idx][i])
+            ans.append(i)
 
             choose(cur_idx + 1)
 
             ans.pop()
             visited[i] = False
-choose(0)
+
+visited[0] = True
+ans.append(0)
+choose(1)
+
+
 print(min_val)
