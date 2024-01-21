562a35da384562020cc099db4b9cbac003de4861
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-21 16:32:57 +0900

    Title: n개의 점 중 m개 고르기, Time: 196ms, Memory: 27MB, Status: Passed - CodeTree

--- 240121/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240121/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -1,4 +1,5 @@
 import sys
+
 n,m = map(int,input().split())
 
 arr = [
@@ -6,41 +7,36 @@ arr = [
     for _ in range(n)
 ]
 
-ans = []
-visited = [False]* (n+1)
-min_val = sys.maxsize
-
 def getDist(a,b):
     x1,y1 = a
     x2,y2 = b
+    return (x1-x2)**2 + (y1-y2) ** 2
 
-    return (x1-x2) ** 2 + (y1-y2) ** 2
-
-def Print():
+def calc():
     max_val = -sys.maxsize
     for i,val in enumerate(ans):
-        distance = 0
         for j,val2 in enumerate(ans):
-            if i == j:
-                continue
-            distance = getDist(val, val2)
-            max_val = max(max_val, distance)
+            if i!=j:
+                max_val = max(max_val, getDist(val,val2))
     return max_val
 
-def choose(idx_num, cnt):
+min_val = sys.maxsize
+ans = []
+
+def choose(idx,cnt):
     global min_val
 
     if cnt == m:
-        min_val = min(min_val, Print())
+        min_val = min(min_val, calc())
         return
-
-    if idx_num == n:
+        
+    if idx == n:
         return
     
-    ans.append(arr[idx_num])
-    choose(idx_num+1,cnt+1)
+    ans.append(arr[idx])
+    choose(idx+1,cnt+1)
     ans.pop()
-    choose(idx_num+1, cnt)
+    choose(idx+1,cnt)
 
 choose(0,0)
 print(min_val)
