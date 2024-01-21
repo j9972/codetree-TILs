4817c72b9ac182c3b20b156a29c7f338f2e7397d
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-21 14:53:05 +0900

    Title: 수들의 합 최대화하기, Time: 277ms, Memory: 27MB, Status: Passed - CodeTree

--- 240121/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 240121/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -9,18 +9,12 @@ max_val = 0
 ans = []
 visited = [False] * (n+1)
 
-def choose(cur_idx, cur_cnt):
+def choose(cur_idx):
     global max_val
 
-    if cur_cnt == n:
-        #print(ans)
-        total = 0
-        for e in ans:
-            total += e
-        max_val = max(max_val, total)
-        return
-    
     if cur_idx == n:
+        #print(ans)
+        max_val = max(max_val, sum(ans))
         return
 
     for i in range(n):
@@ -28,11 +22,10 @@ def choose(cur_idx, cur_cnt):
             visited[i] = True
             ans.append(arr[cur_idx][i])
         
-            choose(cur_idx + 1, cur_cnt + 1)
+            choose(cur_idx + 1)
             ans.pop()
-
-            choose(cur_idx + 1, cur_cnt)
+            
             visited[i] = False
 
-choose(0,0)
+choose(0)
 print(max_val)
