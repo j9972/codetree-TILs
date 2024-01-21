e835cd5397b2faa35aae629c83e6378b5c6dc517
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-21 16:09:16 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 165ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240121/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -0,0 +1,35 @@
+import sys
+
+n = int(input())
+arr = list(map(int,input().split()))
+
+arr.sort()
+
+ans = []
+visited = [False] * (2 * n)
+min_val = sys.maxsize
+
+def choose(idx,cnt):
+    global min_val
+
+    if cnt == n:
+        diff = 0
+        for i in range(2 * n):
+            if visited[i]:
+                diff += arr[i]
+            else:
+                diff -= arr[i]
+        
+        min_val = min(min_val, abs(diff))
+        return
+
+    if idx == 2 * n:
+        return
+    
+    visited[idx] = True
+    choose(idx+1, cnt+1)
+    visited[idx] = False
+    choose(idx+1, cnt)
+
+choose(0,0)
+print(min_val)
