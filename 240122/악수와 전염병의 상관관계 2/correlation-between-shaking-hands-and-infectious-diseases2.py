178ca4934b921b53d45cba0807caa05fcd080c21
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-22 13:53:45 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240122/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -2,12 +2,13 @@ n,k,p,t = map(int,input().split())
 
 infected = [False] * (n+1)
 infected[p] = True
-shake = [0] * n
+shake = [0] * (n+1)
 
 arr = [
     list(map(int,input().split()))
     for _ in range(t)
 ]
+arr.sort(key=lambda x : x[0])
 
 for i in arr:
     _, x,y = i
@@ -22,7 +23,7 @@ for i in arr:
     if infected[y] and shake[y] <= k:
         infected[x] = True
     
-for i in range(n):
+for i in range(1,n+1):
     if infected[i]:
         print(1,end='')
     else:
