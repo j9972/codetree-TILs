4769852fa27735162e0ee0cb611a2fd0af457ce7
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-22 23:40:28 +0900

    Title: 2명의 도둑, Time: 572ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240122/2명의 도둑/two-thieves.py
@@ -0,0 +1,54 @@
+import sys
+
+n,m,c = map(int,input().split())
+
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+max_val = -sys.maxsize
+ans = []
+
+def choose(idx, weight, val):
+    global max_val
+    
+    if idx == m:
+        if weight <= c:
+            max_val = max(max_val, val)
+        return
+    
+    choose(idx+1, weight, val)
+    choose(idx+1, weight + ans[idx], val + ans[idx] ** 2)
+
+def find_max(x,y):
+    global ans, max_val
+
+    ans = arr[x][y:y+m]
+
+    max_val = 0
+    choose(0,0,0)
+    return max_val
+
+def interact(a,b,c,d):
+    return not (b < c or d < a)
+
+def possible(x1,y1,x2,y2):
+    if y1 + m - 1 >= n or y2 + m - 1 >= n: # 둘중하나라도 범위를 벗어나면
+        return False
+    
+    if x1 != x2:
+        return True
+    
+    if interact(y1, y1 + m - 1, y2, y2 + m - 1):
+        return False
+    
+    return True
+
+for x1 in range(n):
+    for y1 in range(n):
+        for x2 in range(n):
+            for y2 in range(n):
+                if possible(x1,y1,x2,y2):
+                    max_val = max(max_val, find_max(x1,y1) + find_max(x2,y2))
+print(max_val)
