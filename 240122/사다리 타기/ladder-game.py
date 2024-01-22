17d91693a449f43f1d832e2d707ba2c87c6de554
정수영 <50689754+j9972@users.noreply.github.com>
2024-01-22 23:15:22 +0900

    Title: 사다리 타기, Time: 202ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240122/사다리 타기/ladder-game.py
+++ 240122/사다리 타기/ladder-game.py
@@ -1,26 +1,26 @@
 n,m = map(int,input().split())
 
-arr = []
-for i in range(m):
+arr = list()
+for _ in range(m):
     a,b = tuple(map(int,input().split()))
     arr.append((b, a-1))
 
 arr.sort()
 
 min_val = m
-ans = []
+ans = list()
 
 def possible():
-    pp1 = [i for i in range(n)]
-    pp2 = [i for i in range(n)]
+    ppl1 = [i for i in range(n)]
+    ppl2 = [i for i in range(n)]
 
-    for _ , idx in arr:
-        pp1[idx], pp1[idx+1] = pp1[idx+1], pp1[idx]
-    for _ , idx in ans:
-        pp2[idx], pp2[idx+1] = pp2[idx+1], pp2[idx]
-    
+    for _, idx in arr:
+        ppl1[idx], ppl1[idx+1] = ppl1[idx+1], ppl1[idx]
+    for _, idx in ans:
+        ppl1[idx], ppl1[idx+1] = ppl1[idx+1], ppl1[idx]
+
     for i in range(n):
-        if pp1[i] != pp2[i]:
+        if ppl1[i] != ppl2[i]:
             return False
     
     return True
@@ -36,7 +36,7 @@ def choose(cnt):
     ans.append(arr[cnt])
     choose(cnt+1)
     ans.pop()
-    choose(cnt+1) # 선택안된 줄에 대해서 실행을 한번더
+    choose(cnt+1)
 
 choose(0)
 print(min_val)
