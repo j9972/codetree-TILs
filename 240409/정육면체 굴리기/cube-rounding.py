29e10c1fd4247a153020d69722080d1691f423ca
정수영 <50689754+j9972@users.noreply.github.com>
2024-04-09 15:34:21 +0900

    Title: 정육면체 굴리기, Time: 238ms, Memory: 25MB, Status: Passed - CodeTree

--- 240409/정육면체 굴리기/cube-rounding.py
+++ 240409/정육면체 굴리기/cube-rounding.py
@@ -11,35 +11,9 @@ arr = [
 direction = list(map(int,input().split()))
 dxs,dys = [0,0,-1,1],[1,-1,0,0] # 동서북남 순서
 
-cube = [[0],
-        [0, 0, 0, 0],
-        [0]]
+dice = [0 for _ in range(7)]
 
-def move_cube(cube, d):
-    new_cube = copy.deepcopy(cube)
-
-    if d == 0: # RIGHT
-        new_cube[0][0] = cube[1][0]
-        new_cube[1][0] = cube[2][0]
-        new_cube[1][2] = cube[0][0]
-        new_cube[2][0] = cube[1][2]
-    elif d == 1: # LEFT
-        new_cube[0][0] = cube[1][2]
-        new_cube[1][0] = cube[0][0]
-        new_cube[1][2] = cube[2][0]
-        new_cube[2][0] = cube[1][0]
-    elif d == 2: # UP
-        new_cube[0][0] = cube[1][1]
-        new_cube[1][1] = cube[2][0]
-        new_cube[1][3] = cube[0][0]
-        new_cube[2][0] = cube[1][3]
-    else: # DOWN
-        new_cube[0][0] = cube[1][3]
-        new_cube[1][1] = cube[0][0]
-        new_cube[1][3] = cube[2][0]
-        new_cube[2][0] = cube[1][1]
-
-    return new_cube
+up, front, right = 1,2,3
 
 def in_range(x,y):
     return 0<=x<n and 0<=y<m
@@ -58,14 +32,23 @@ for d in direction:
         q.append((x,y))
         continue
 
-    cube = move_cube(cube, d)
+    if d == 0:
+        up, front, right = 7-right,front, up
+    elif d == 1:
+        up, front, right = right,front, 7-up
+    elif d == 2:
+        up, front, right = front, 7-up, right
+    elif d == 3:
+        up, front, right = 7 - front, up, right
+    
+    bottom = 7 - up
 
     if arr[nx][ny] == 0:
-        arr[nx][ny] = cube[2][0]
+        arr[nx][ny] = dice[bottom]
     else:
-        cube[2][0] = arr[nx][ny]
+        dice[bottom] = arr[nx][ny]
         arr[nx][ny] = 0
 
     q.append((nx,ny))
 
-    print(cube[0][0])
+    print(dice[up])
