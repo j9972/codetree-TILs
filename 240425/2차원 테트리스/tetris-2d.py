7c0ed7aa74e61bc6d7e833e9bc5e5896293c1996
정수영 <50689754+j9972@users.noreply.github.com>
2024-04-25 11:35:10 +0900

    Title: 2차원 테트리스, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240425/2차원 테트리스/tetris-2d.py
+++ 240425/2차원 테트리스/tetris-2d.py
@@ -1,12 +1,4 @@
-n, m, score, k = 6, 4, 0, int(input())
-
-arr = [
-    [
-        [0 for _ in range(4)]
-        for _ in range(6)
-    ]
-    for _ in range(2)
-]
+n,m,score,k = 6,4,0,int(input())
 
 shape = [
     [],
@@ -27,82 +19,33 @@ shape = [
     ]
 ]
 
+arr =[
+    [
+        [0 for _ in range(m)]
+        for _ in range(n)
+    ]
+    for _ in range(2)
+]
+
 def in_range(x,y):
     return 0<=x<n and 0<=y<m
 
-def can_go(t,x,y,color):
+def can_go(num, t, x, y):
     for dx in range(2):
         for dy in range(2):
             if shape[t][dx][dy]:
                 nx,ny = x + dx, y + dy
 
-                if not in_range(nx,ny) or arr[color][nx][ny]:
+                if not in_range(nx,ny) or arr[num][nx][ny] == 1:
                     return False
     return True
 
-def put(color, t,x,y):
+def put(num, t, x, y):
     for dx in range(2):
         for dy in range(2):
             if shape[t][dx][dy]:
                 nx,ny = x + dx, y + dy
-                arr[color][nx][ny] = 1
-
-def full_line(color, row):
-    return all([
-        arr[color][row][col] == 1
-        for col in range(m)
-    ])
-
-def drop_one_line(color, end_row):
-    for i in range(end_row,0,-1):
-        for j in range(m):
-            arr[color][i][j] = arr[color][i-1][j]
-            arr[color][i-1][j] = 0
-
-def exist_block(color, row):
-    return any([
-        arr[color][row][col] == 1
-        for col in range(m)
-    ])
-
-def dark(color):
-    global score
-
-    row = n-1
-    while (row>=2):
-        if full_line(color, row):
-            score += 1
-            drop_one_line(color, row)
-        else:
-            row -= 1
-
-def light(color):
-    cnt = 0
-    for i in range(2):
-        if exist_block(color, i):
-            cnt += 1
-    
-    for _ in range(cnt):
-        drop_one_line(color, n-1)
-
-def drop(color, t, col):
-    for row in range(n):
-        if not can_go(t, row + 1, col, color):
-            put(color, t, row, col)
-            break
-    
-    dark(color)
-    light(color)
-
-def simulate(t,x,y):
-    drop(0,t,y)
-
-    if t == 1:
-        drop(1,1,m-1-x)
-    elif t == 2:
-        drop(1,3,m-1-x)
-    elif t == 3:
-        drop(1,2,m-1-(x+1))
+                arr[num][nx][ny] = 1
 
 def remain():
     return sum([
@@ -112,7 +55,63 @@ def remain():
         for j in range(m)
     ])
 
-for _ in range(k):
+def full_line(num, row):
+    return all([
+        arr[num][row][col] == 1
+        for col in range(m)
+    ])
+
+def exist_block(num, row):
+    return any([
+        arr[num][row][col] == 1
+        for col in range(m)
+    ])
+
+def drop_line(num, end_row):
+    for i in range(end_row, 0,-1):
+        for j in range(m):
+            arr[num][i][j] = arr[num][i-1][j]
+            arr[num][i-1][j] = 0
+
+def dark(num):
+    global score 
+
+    for i in range(n-1, 1,-1):
+        if full_line(num, i):
+            score += 1
+            drop_line(num, i)
+
+def light(num):
+    cnt = 0
+    for i in range(2):
+        if exist_block(num, i):
+            cnt += 1
+        
+    for i in range(cnt):
+        drop_line(num,n-1)
+
+
+def drop(num, t, y):
+    for x in range(n):
+        if not can_go(num, t, x+1, y):
+            put(num, t, x, y)
+            break
+    
+    dark(num)
+    light(num)
+
+
+def simulate(t, x, y):
+    drop(0, t, y)
+
+    if t == 1:
+        drop(1,1,m-1-x)
+    elif t == 2:
+        drop(1,3,m-1-x)
+    else:
+        drop(1,2,m-1-(x+1))
+
+for i in range(k):
     t,x,y = map(int,input().split())
     simulate(t,x,y)
 
